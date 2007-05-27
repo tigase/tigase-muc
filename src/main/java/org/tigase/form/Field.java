8bf6ab174db14b126d910e40d40b40a00249dc3c
Bartosz Malkowski <bmalkowski@tigase.pl>
2007-05-27 10:09:01 +0000

    Add XEP-0004  (Data Forms) implementation
    git-svn-id: file:///home/svn/repos/tigase-muc/trunk@27 5cc3df33-c827-0410-82ff-b820e365a07a

---
+++ src/main/java/org/tigase/form/Field.java
@@ -0,0 +1,326 @@
+/*  tigase-muc
+ *  Copyright (C) 2007 by Bartosz M. Małkowski
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software Foundation,
+ *  Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ *  $Id$
+ */
+package org.tigase.form;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import tigase.xml.Element;
+
+/**
+ * 
+ * <p>
+ * Created: 2007-05-27 10:56:06
+ * </p>
+ * 
+ * @author bmalkow
+ * @version $Rev$
+ */
+public class Field {
+    public static Field fieldBoolean(String label, String var, Boolean value) {
+        Field field = new Field("boolean");
+        field.label = label;
+        field.var = var;
+
+        if (value != null && value) {
+            field.values = new String[] { "1" };
+        } else if (value != null && !value) {
+            field.values = new String[] { "0" };
+        }
+        return field;
+    }
+
+    public static Field fieldFixed(String value) {
+        Field field = new Field("fixed");
+        field.values = new String[] { value };
+        return field;
+    }
+
+    public static Field fieldHidden(String var, String value) {
+        Field field = new Field("hidden", var);
+        field.values = new String[] { value };
+        return field;
+    }
+
+    public static Field fieldListMulti(String label, String var, String[] values, String[] optionsLabel,
+            String optionsValue) {
+        if (optionsLabel.length != optionsValue.length()) {
+            throw new RuntimeException("Invalid optionsLabel and optinsValue length");
+        }
+        Field field = new Field("list-single", var);
+        field.label = label;
+        field.values = values;
+        field.optionLabels = optionsLabel;
+        field.optionValues = optionsLabel;
+        return field;
+    }
+
+    public static Field fieldListSingle(String label, String var, String value, String[] optionsLabel,
+            String optionsValue) {
+        if (optionsLabel.length != optionsValue.length()) {
+            throw new RuntimeException("Invalid optionsLabel and optinsValue length");
+        }
+        Field field = new Field("list-single", var);
+        field.label = label;
+        field.values = new String[] { value };
+        field.optionLabels = optionsLabel;
+        field.optionValues = optionsLabel;
+        return field;
+    }
+
+    public static Field fieldTextMulti(String label, String var, String value) {
+        Field field = new Field("text-multi", var);
+        field.label = label;
+        field.values = new String[] { value };
+        return field;
+    }
+
+    public static Field fieldTextPrivate(String label, String var, String value) {
+        Field field = new Field("text-private", var);
+        field.label = label;
+        field.values = new String[] { value };
+        return field;
+    }
+
+    public static Field fieldTextSingle(String label, String var, String value) {
+        Field field = new Field("text-single", var);
+        field.label = label;
+        field.values = new String[] { value };
+        return field;
+    }
+
+    private String description;
+
+    private String label;
+
+    private String[] optionLabels;
+
+    private String[] optionValues;
+
+    private boolean required;
+
+    private String type;
+
+    private String[] values;
+
+    private String var;
+
+    public Field(Element fieldElement) {
+        this.var = fieldElement.getAttribute("var");
+        this.type = fieldElement.getAttribute("type");
+        this.label = fieldElement.getAttribute("label");
+        Element d = fieldElement.getChild("desc");
+        if (d != null) {
+            this.description = d.getCData();
+        }
+        this.required = fieldElement.getChild("required") != null;
+
+        List<String> valueList = new LinkedList<String>();
+        List<String> optionsLabelList = new LinkedList<String>();
+        List<String> optionsValueList = new LinkedList<String>();
+
+        for (Element element : fieldElement.getChildren()) {
+            if ("value".equals(element.getName())) {
+                valueList.add(element.getCData());
+            } else if ("value".equals(element.getName())) {
+                optionsLabelList.add(element.getAttribute("label"));
+                Element v = element.getChild("value");
+                optionsValueList.add(v.getCData());
+            }
+        }
+        this.values = valueList.toArray(new String[] {});
+        this.optionLabels = optionsLabelList.toArray(new String[] {});
+        this.optionValues = optionsValueList.toArray(new String[] {});
+    }
+
+    private Field(String type) {
+        this.type = type;
+    }
+
+    private Field(String type, String var) {
+        this.type = type;
+    }
+
+    /**
+     * @return Returns the description.
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    public Element getElement() {
+        Element field = new Element("field");
+        if (this.var != null) {
+            field.setAttribute("var", var);
+        }
+        if (this.type != null) {
+            field.setAttribute("type", this.type);
+        }
+        if (this.label != null) {
+            field.setAttribute("label", this.label);
+        }
+
+        if (this.description != null) {
+            field.addChild(new Element("desc", this.description));
+        }
+
+        if (this.required) {
+            field.addChild(new Element("required"));
+        }
+
+        if (this.values != null) {
+            for (String value : this.values) {
+                field.addChild(new Element("value", value));
+            }
+        }
+
+        if (this.optionLabels != null) {
+            for (int i = 0; i < this.optionLabels.length; i++) {
+                Element option = new Element("option");
+                option.setAttribute("label", this.optionLabels[i]);
+                Element vo = new Element("value", this.optionValues[i]);
+                option.addChild(vo);
+                field.addChild(option);
+            }
+        }
+
+        return field;
+    }
+
+    /**
+     * @return Returns the label.
+     */
+    public String getLabel() {
+        return label;
+    }
+
+    /**
+     * @return Returns the optionLabels.
+     */
+    public String[] getOptionLabels() {
+        return optionLabels;
+    }
+
+    /**
+     * @return Returns the optionValues.
+     */
+    public String[] getOptionValues() {
+        return optionValues;
+    }
+
+    /**
+     * @return Returns the type.
+     */
+    public String getType() {
+        return type;
+    }
+
+    public String getValue() {
+        if (this.values != null && this.values.length > 0) {
+            return this.values[0];
+        } else
+            return null;
+    }
+
+    /**
+     * @return Returns the values.
+     */
+    public String[] getValues() {
+        return values;
+    }
+
+    /**
+     * @return Returns the var.
+     */
+    public String getVar() {
+        return var;
+    }
+
+    /**
+     * @return Returns the required.
+     */
+    public boolean isRequired() {
+        return required;
+    }
+
+    /**
+     * @param description
+     *            The description to set.
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    /**
+     * @param label
+     *            The label to set.
+     */
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    /**
+     * @param optionLabels
+     *            The optionLabels to set.
+     */
+    public void setOptionLabels(String[] optionLabels) {
+        this.optionLabels = optionLabels;
+    }
+
+    /**
+     * @param optionValues
+     *            The optionValues to set.
+     */
+    public void setOptionValues(String[] optionValues) {
+        this.optionValues = optionValues;
+    }
+
+    /**
+     * @param required
+     *            The required to set.
+     */
+    public void setRequired(boolean required) {
+        this.required = required;
+    }
+
+    /**
+     * @param type
+     *            The type to set.
+     */
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    /**
+     * @param values
+     *            The values to set.
+     */
+    public void setValues(String[] values) {
+        this.values = values;
+    }
+
+    /**
+     * @param var
+     *            The var to set.
+     */
+    public void setVar(String var) {
+        this.var = var;
+    }
+}
---
+++ src/main/java/org/tigase/form/Form.java
@@ -0,0 +1,171 @@
+/*  tigase-muc
+ *  Copyright (C) 2007 by Bartosz M. Małkowski
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software Foundation,
+ *  Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ *  $Id$
+ */
+package org.tigase.form;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import tigase.xml.Element;
+
+/**
+ * 
+ * <p>
+ * Created: 2007-05-27 11:41:02
+ * </p>
+ * 
+ * @author bmalkow
+ * @version $Rev$
+ */
+public class Form {
+
+    private List<Field> fields = new LinkedList<Field>();
+
+    private Map<String, Field> fieldsByVar = new HashMap<String, Field>();
+    private String instruction;
+    private String title;
+
+    private String type;
+
+    public Form(Element form) {
+        this.type = form.getAttribute("type");
+        for (Element sub : form.getChildren()) {
+            if ("title".equals(sub.getName())) {
+                this.title = sub.getCData();
+            } else if ("instructions".equals(sub.getName())) {
+                this.instruction = sub.getCData();
+            } else if ("field".equals(sub.getName())) {
+                Field field = new Field(sub);
+                this.fields.add(field);
+                this.fieldsByVar.put(field.getVar(), field);
+            }
+        }
+    }
+
+    public Form(String type, String title, String instruction) {
+        this.type = type;
+        this.title = title;
+        this.instruction = instruction;
+    }
+
+    public void addField(Field field) {
+        this.fields.add(field);
+    }
+
+    public Field get(String var) {
+        return this.fieldsByVar.get(var);
+    }
+
+    public Boolean getAsBoolean(String var) {
+        Field f = get(var);
+        if (f != null) {
+            String v = f.getValue();
+            if (v == null) {
+                return null;
+            } else if ("1".equals(v)) {
+                return Boolean.TRUE;
+            } else {
+                return Boolean.FALSE;
+            }
+        } else
+            return null;
+    }
+
+    public String getAsString(String var) {
+        Field f = get(var);
+        if (f != null) {
+            String v = f.getValue();
+            return v;
+        } else
+            return null;
+    }
+
+    public String[] getAsStrings(String var) {
+        Field f = get(var);
+        if (f != null) {
+            String[] v = f.getValues();
+            return v;
+        } else
+            return null;
+    }
+
+    public Element getElement() {
+        Element form = new Element("x");
+        form.setAttribute("xmlns", "jabber:x:data");
+        form.setAttribute("type", type);
+
+        if (this.title != null) {
+            form.addChild(new Element("title", this.title));
+        }
+        if (this.instruction != null) {
+            form.addChild(new Element("instructions", this.instruction));
+        }
+        for (Field field : this.fields) {
+            form.addChild(field.getElement());
+        }
+        return form;
+    }
+
+    /**
+     * @return Returns the instruction.
+     */
+    public String getInstruction() {
+        return instruction;
+    }
+
+    /**
+     * @return Returns the title.
+     */
+    public String getTitle() {
+        return title;
+    }
+
+    /**
+     * @return Returns the type.
+     */
+    public String getType() {
+        return type;
+    }
+
+    /**
+     * @param instruction
+     *            The instruction to set.
+     */
+    public void setInstruction(String instruction) {
+        this.instruction = instruction;
+    }
+
+    /**
+     * @param title
+     *            The title to set.
+     */
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    /**
+     * @param type
+     *            The type to set.
+     */
+    public void setType(String type) {
+        this.type = type;
+    }
+}
