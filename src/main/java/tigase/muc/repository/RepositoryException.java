6d6cc66ead6d4c5a25e5479941268a818426df38
Wojciech Kapcia <wojtek@tigase.org>
2012-08-20 22:23:15 +0000

    licence updates
    git-svn-id: file:///home/svn/repos/tigase-muc/trunk@270 5cc3df33-c827-0410-82ff-b820e365a07a

--- src/main/java/tigase/muc/repository/RepositoryException.java
+++ src/main/java/tigase/muc/repository/RepositoryException.java
@@ -3,15 +3,15 @@
  * Copyright (C) 2007 "Bartosz M. Małkowski" <bartosz.malkowski@tigase.org>
  *
  * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
+ * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * GNU Affero General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
+ * You should have received a copy of the GNU Affero General Public License
  * along with this program. Look for COPYING file in the top folder.
  * If not, see http://www.gnu.org/licenses/.
  *
