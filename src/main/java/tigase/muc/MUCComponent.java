b78c45f1d57444eb4eb7218f6b06ab15c52eacc6
Andrzej Wójcik <andrzej@hi-low.eu>
2012-11-15 19:40:28 +0100

    Fix for service discovery issues

--- src/main/java/tigase/muc/MUCComponent.java
+++ src/main/java/tigase/muc/MUCComponent.java
@@ -346,6 +346,12 @@ public class MUCComponent extends AbstractComponent<MucConfig> implements DelDel
 		binds.put(MUC_REPOSITORY_VAR, mucRepository);
 	}
 
+        @Override
+        public boolean isSubdomain() {
+                return true;
+        }
+        
+        
 	@Override
 	public int processingInThreads() {
 		return Runtime.getRuntime().availableProcessors();
