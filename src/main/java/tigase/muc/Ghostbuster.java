2b842e295b176923c26c9f47f65e38860b8d1c96
Bartosz Malkowski <bmalkowski@tigase.pl>
2013-03-20 18:36:37 +0100

    #1131 - Tigase chooses wrong component to route traffic to

--- src/main/java/tigase/muc/Ghostbuster.java
+++ src/main/java/tigase/muc/Ghostbuster.java
@@ -20,41 +20,95 @@
  *
  */
 
-
-
 package tigase.muc;
 
-//~--- non-JDK imports --------------------------------------------------------
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import tigase.muc.modules.PresenceModule;
-
 import tigase.server.Packet;
 import tigase.server.ReceiverTimeoutHandler;
-
 import tigase.util.TigaseStringprepException;
-
 import tigase.xml.Element;
-
 import tigase.xmpp.JID;
 
 //~--- JDK imports ------------------------------------------------------------
 
-import java.util.Collections;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.TimeUnit;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Set;
-
 /**
  * @author bmalkow
- *
+ * 
  */
 public class Ghostbuster {
+	private static class JIDDomain {
+		private String cacheKey;
+		private final String domain;
+		private String lastUsedRoomName;
+		private final JID source;
+
+		/**
+		 * 
+		 * 
+		 * @param source
+		 * @param domain
+		 */
+		public JIDDomain(JID source, String domain) {
+			this.source = source;
+			this.domain = domain;
+			cacheKey = ((this.source == null) ? "-" : this.source.toString()) + ":"
+					+ ((this.domain == null) ? "-" : this.domain.toString());
+		}
+
+		/**
+		 * Method description
+		 * 
+		 * 
+		 * @param obj
+		 * 
+		 * @return
+		 */
+		@Override
+		public boolean equals(Object obj) {
+			if (obj == this) {
+				return true;
+			}
+			if (!(obj instanceof JIDDomain)) {
+				return false;
+			}
+
+			return cacheKey.equals(((JIDDomain) obj).cacheKey);
+		}
+
+		/**
+		 * Method description
+		 * 
+		 * 
+		 * @return
+		 */
+		@Override
+		public int hashCode() {
+			return cacheKey.hashCode();
+		}
+
+		/**
+		 * Method description
+		 * 
+		 * 
+		 * @return
+		 */
+		@Override
+		public String toString() {
+			return cacheKey;
+		}
+	}
+
 	private static final Set<String> intReasons = new HashSet<String>() {
 		private static final long serialVersionUID = 1L;
 		{
@@ -70,26 +124,21 @@ public class Ghostbuster {
 	/** Field description */
 	public static final Set<String> R = Collections.unmodifiableSet(intReasons);
 
-	//~--- fields ---------------------------------------------------------------
-
-	private final Map<JIDDomain, Long> lastActivity = new ConcurrentHashMap<JIDDomain,
-																											Long>();
-
+	private long idCounter;
+	private final Map<JIDDomain, Long> lastActivity = new ConcurrentHashMap<JIDDomain, Long>();
 	/** Field description */
 	protected Logger log = Logger.getLogger(this.getClass().getName());
-	private long idCounter;
 	private final MUCComponent mucComponent;
 	private final ReceiverTimeoutHandler pingHandler;
-	private PresenceModule presenceModule;
 
-	//~--- constructors ---------------------------------------------------------
+	private PresenceModule presenceModule;
 
 	/**
 	 * @param mucComponent
 	 */
 	public Ghostbuster(MUCComponent mucComponent) {
 		this.mucComponent = mucComponent;
-		this.pingHandler  = new ReceiverTimeoutHandler() {
+		this.pingHandler = new ReceiverTimeoutHandler() {
 			@Override
 			public void responseReceived(Packet data, Packet response) {
 				try {
@@ -100,12 +149,12 @@ public class Ghostbuster {
 					}
 				}
 			}
+
 			@Override
 			public void timeOutExpired(Packet data) {
 				try {
 					if (log.isLoggable(Level.FINEST)) {
-						log.finest("Received ping timeout for ping " +
-											 data.getElement().getAttributeStaticStr("id"));
+						log.finest("Received ping timeout for ping " + data.getElement().getAttributeStaticStr("id"));
 					}
 					onPingTimeout(data.getStanzaFrom().getDomain(), data.getStanzaTo());
 				} catch (Exception e) {
@@ -117,15 +166,13 @@ public class Ghostbuster {
 		};
 	}
 
-	//~--- methods --------------------------------------------------------------
-
 	/**
 	 * @param packet
 	 */
 	private String checkError(final Packet packet) {
 		final String type = packet.getElement().getAttributeStaticStr(Packet.TYPE_ATT);
 
-		if ((type == null) ||!type.equals("error")) {
+		if ((type == null) || !type.equals("error")) {
 			return null;
 		}
 
@@ -135,8 +182,7 @@ public class Ghostbuster {
 			return null;
 		}
 		for (Element reason : errorElement.getChildren()) {
-			if ((reason.getXMLNS() == null) ||
-					!reason.getXMLNS().equals("urn:ietf:params:xml:ns:xmpp-stanzas")) {
+			if ((reason.getXMLNS() == null) || !reason.getXMLNS().equals("urn:ietf:params:xml:ns:xmpp-stanzas")) {
 				continue;
 			}
 			if (Ghostbuster.R.contains(reason.getName())) {
@@ -147,37 +193,31 @@ public class Ghostbuster {
 		return null;
 	}
 
-	//~--- get methods ----------------------------------------------------------
-
 	/**
 	 * Method description
-	 *
-	 *
+	 * 
+	 * 
 	 * @return
 	 */
 	public PresenceModule getPresenceModule() {
 		return presenceModule;
 	}
 
-	//~--- methods --------------------------------------------------------------
-
 	/**
-	 *
+	 * 
 	 * @param response
 	 * @throws TigaseStringprepException
 	 */
 	protected void onPingReceived(final Packet response) throws TigaseStringprepException {
-		final JIDDomain k = new JIDDomain(response.getStanzaFrom(),
-																			response.getStanzaTo().getDomain());
+		final JIDDomain k = new JIDDomain(response.getStanzaFrom(), response.getStanzaTo().getDomain());
 
 		if (lastActivity.containsKey(k)) {
 			final String errorCause = checkError(response);
 
 			if (errorCause != null) {
 				if (log.isLoggable(Level.FINEST)) {
-					log.finest("Received error response for ping " +
-										 response.getElement().getAttributeStaticStr(Packet.ID_ATT) + "(" +
-										 checkError(response) + ") of" + k);
+					log.finest("Received error response for ping " + response.getElement().getAttributeStaticStr(Packet.ID_ATT)
+							+ "(" + checkError(response) + ") of" + k);
 				}
 				processError(k);
 			} else {
@@ -205,16 +245,16 @@ public class Ghostbuster {
 
 	/**
 	 * @throws TigaseStringprepException
-	 *
+	 * 
 	 */
 	public void ping() throws TigaseStringprepException {
 		if (log.isLoggable(Level.FINE)) {
 			log.log(Level.FINE, "Pinging 1000 known jids");
 		}
 
-		int c                               = 0;
-		final long now                      = System.currentTimeMillis();
-		final long border                   = now - 1000 * 60 * 60;
+		int c = 0;
+		final long now = System.currentTimeMillis();
+		final long border = now - 1000 * 60 * 60;
 		Iterator<Entry<JIDDomain, Long>> it = lastActivity.entrySet().iterator();
 
 		while (it.hasNext() && (c < 1000)) {
@@ -222,24 +262,24 @@ public class Ghostbuster {
 
 			if (entry.getValue() < border) {
 				++c;
-				ping(entry.getKey().domain, entry.getKey().source);
+				ping(entry.getKey().lastUsedRoomName, entry.getKey().domain, entry.getKey().source);
 			}
 		}
 	}
 
-	private void ping(String fromDomain, JID jid) throws TigaseStringprepException {
+	private void ping(String roomName, String fromDomain, JID jid) throws TigaseStringprepException {
 		final String id = "png-" + (++idCounter);
 
 		if (log.isLoggable(Level.FINER)) {
 			log.log(Level.FINER, "Pinging " + jid + ". id=" + id);
 		}
 
-		Element ping = new Element("iq", new String[] { "type", "id", "from", "to" },
-															 new String[] { "get",
-						id, fromDomain, jid.toString() });
+		final String from = (roomName != null ? (roomName + "@") : "") + fromDomain;
 
-		ping.addChild(new Element("ping", new String[] { "xmlns" },
-															new String[] { "urn:xmpp:ping" }));
+		Element ping = new Element("iq", new String[] { "type", "id", "from", "to" }, new String[] { "get", id, from,
+				jid.toString() });
+
+		ping.addChild(new Element("ping", new String[] { "xmlns" }, new String[] { "urn:xmpp:ping" }));
 
 		Packet packet = Packet.packetInstance(ping);
 
@@ -265,36 +305,33 @@ public class Ghostbuster {
 		}
 	}
 
-	//~--- set methods ----------------------------------------------------------
-
 	/**
 	 * Method description
-	 *
-	 *
+	 * 
+	 * 
 	 * @param presenceModule
 	 */
 	public void setPresenceModule(PresenceModule presenceModule) {
 		this.presenceModule = presenceModule;
 	}
 
-	//~--- methods --------------------------------------------------------------
-
 	/**
 	 * Method description
-	 *
-	 *
+	 * 
+	 * 
 	 * @param packet
-	 *
+	 * 
 	 * @throws TigaseStringprepException
 	 */
 	public void update(Packet packet) throws TigaseStringprepException {
-		if ((packet.getStanzaFrom() == null) ||
-				(packet.getStanzaFrom().getResource() == null)) {
+		if ((packet.getStanzaFrom() == null) || (packet.getStanzaFrom().getResource() == null)) {
 			return;
 		}
 
-		final JIDDomain k = new JIDDomain(packet.getStanzaFrom(),
-																			packet.getStanzaTo().getDomain());
+		final JIDDomain k = new JIDDomain(packet.getStanzaFrom(), packet.getStanzaTo().getDomain());
+
+		k.lastUsedRoomName = packet.getStanzaTo().getLocalpart();
+
 		final String type = packet.getElement().getAttributeStaticStr(Packet.TYPE_ATT);
 
 		if (checkError(packet) != null) {
@@ -302,14 +339,13 @@ public class Ghostbuster {
 				log.finest("Received presence error: " + packet.getElement().toString());
 			}
 			processError(k);
-		} else if ("presence".equals(packet.getElemName()) && (type != null) &&
-							 type.equals("unavailable")) {
+		} else if ("presence".equals(packet.getElemName()) && (type != null) && type.equals("unavailable")) {
 			if (log.isLoggable(Level.FINEST)) {
 				log.finest("Removal last activity of " + k);
 			}
 			this.lastActivity.remove(k);
-		} else if (!lastActivity.containsKey(k) && "presence".equals(packet.getElemName()) &&
-							 ((type == null) ||!type.equals("error"))) {
+		} else if (!lastActivity.containsKey(k) && "presence".equals(packet.getElemName())
+				&& ((type == null) || !type.equals("error"))) {
 			if (log.isLoggable(Level.FINEST)) {
 				log.finest("Creation last activity entry for " + k);
 			}
@@ -324,77 +360,4 @@ public class Ghostbuster {
 			lastActivity.put(k, System.currentTimeMillis());
 		}
 	}
-
-	//~--- inner classes --------------------------------------------------------
-
-	private static class JIDDomain {
-		private String cacheKey;
-		private final String domain;
-		private final JID source;
-
-		//~--- constructors -------------------------------------------------------
-
-		/**
-		 *
-		 *
-		 * @param source
-		 * @param domain
-		 */
-		public JIDDomain(JID source, String domain) {
-			this.source = source;
-			this.domain = domain;
-			cacheKey    = ((this.source == null)
-										 ? "-"
-										 : this.source.toString()) + ":" + ((this.domain == null)
-							? "-"
-							: this.domain.toString());
-		}
-
-		//~--- methods ------------------------------------------------------------
-
-		/**
-		 * Method description
-		 *
-		 *
-		 * @param obj
-		 *
-		 * @return
-		 */
-		@Override
-		public boolean equals(Object obj) {
-			if (obj == this) {
-				return true;
-			}
-			if (!(obj instanceof JIDDomain)) {
-				return false;
-			}
-
-			return cacheKey.equals(((JIDDomain) obj).cacheKey);
-		}
-
-		/**
-		 * Method description
-		 *
-		 *
-		 * @return
-		 */
-		@Override
-		public int hashCode() {
-			return cacheKey.hashCode();
-		}
-
-		/**
-		 * Method description
-		 *
-		 *
-		 * @return
-		 */
-		@Override
-		public String toString() {
-			return cacheKey;
-		}
-	}
 }
-
-
-//~ Formatted in Tigase Code Convention on 13/02/20
