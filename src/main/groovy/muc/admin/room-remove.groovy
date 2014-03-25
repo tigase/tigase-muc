5f13070d792c459620d322ae4bb09519540a9db9
Bartosz Malkowski <bmalkowski@tigase.pl>
2014-03-25 08:25:39 +0100

    #1810: Exception in room-remove groovy script

--- src/main/groovy/muc/admin/room-remove.groovy
+++ src/main/groovy/muc/admin/room-remove.groovy
@@ -15,20 +15,26 @@ def reason = Command.getFieldValue(p, REASON_KEY)
 def alternateJid = Command.getFieldValue(p, ALTERNATE_JID_KEY)
 
 if (roomName == null) {
-  // No data to process, let's ask user to provide
-  // a list of words 
-  def res = (Packet)p.commandResult(Command.DataType.form)
-  Command.addFieldValue(res, ROOM_NAME_KEY, "", "text-single", "Room name")
-  Command.addFieldValue(res, REASON_KEY, "", "text-single", "Reason")
-  Command.addFieldValue(res, ALTERNATE_JID_KEY, "", "jid-single", "Alternate room")
-  return res
+	// No data to process, let's ask user to provide
+	// a list of words
+	def res = (Packet)p.commandResult(Command.DataType.form)
+	Command.addFieldValue(res, ROOM_NAME_KEY, "", "text-single", "Room name")
+	Command.addFieldValue(res, REASON_KEY, "", "text-single", "Reason")
+	Command.addFieldValue(res, ALTERNATE_JID_KEY, "", "jid-single", "Alternate room")
+	return res
 }
 
 if (roomName != null){
-    tigase.xmpp.BareJID jid = tigase.xmpp.BareJID.bareJIDInstance(roomName+"@"+p.getStanzaTo().getBareJID().toString());
+	tigase.xmpp.BareJID jid;
+	try{
+		jid = tigase.xmpp.BareJID.bareJIDInstance(roomName+"@"+p.getStanzaTo().getBareJID().getDomain());
+	}catch(tigase.util.TigaseStringprepException e){
+		jid = tigase.xmpp.BareJID.bareJIDInstance(roomName);
+	}
+
 	def room=mucRepository.getRoom(jid)
 	if(room==null)
-		return "Room doesn't exists"
+		return "Room "+jid+" doesn't exists"
 	ownerModule.destroy(room, alternateJid, reason);
 	return "Room "+room.getRoomJID()+" removed";
 }
