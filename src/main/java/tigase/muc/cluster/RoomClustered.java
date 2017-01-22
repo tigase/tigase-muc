c76983180b7d479e2b05911fa080e01c8ff2ac9e
Andrzej Wójcik <andrzejw@tigase.org>
2017-01-22 13:40:45 +0100

    Task #4358 - fixed startup of a component

--- src/main/java/tigase/muc/cluster/RoomClustered.java
+++ src/main/java/tigase/muc/cluster/RoomClustered.java
@@ -26,7 +26,7 @@ import java.util.concurrent.ConcurrentMap;
  */
 public class RoomClustered<ID> extends RoomWithId<ID> {
 
-	@Bean(name = "roomFactory", parent = MUCComponentClustered.class)
+	@Bean(name = "roomFactory", parent = MUCComponentClustered.class, exportable = true)
 	public static class RoomFactoryImpl implements RoomFactory {
 
 		@Override
