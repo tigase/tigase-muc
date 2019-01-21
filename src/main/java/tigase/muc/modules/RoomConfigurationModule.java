4737619df6e176995c47918ea3484913a2764601
Wojciech Kapcia <wojtek@tigase.org>
2019-01-21 15:46:28 -0300

    #8483 update licence headers

--- src/main/java/tigase/muc/modules/RoomConfigurationModule.java
+++ src/main/java/tigase/muc/modules/RoomConfigurationModule.java
@@ -1,13 +1,10 @@
-/*
- * RoomConfigurationModule.java
- *
- * Tigase Workgroup Queues Component
- * Copyright (C) 2011-2016 "Tigase, Inc." <office@tigase.com>
+/**
+ * Tigase MUC - Multi User Chat component for Tigase
+ * Copyright (C) 2007 Tigase, Inc. (office@tigase.com)
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License,
- * or (at your option) any later version.
+ * the Free Software Foundation, version 3 of the License.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -17,9 +14,7 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program. Look for COPYING file in the top folder.
  * If not, see http://www.gnu.org/licenses/.
- *
  */
-
 package tigase.muc.modules;
 
 import tigase.component.exceptions.RepositoryException;
