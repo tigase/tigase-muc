f9a9ec479c3918cb83ee4711f7027113ef16ebb0
Bartosz Malkowski <bmalkowski@tigase.pl>
2019-09-25 14:22:59 +0200

    muc-122: XEP-0410: MUC Self-Ping (Schrödinger's Chat)
    update copyrights

--- src/test/java/tigase/muc/modules/selfping/SelfPingerMonitorTest.java
+++ src/test/java/tigase/muc/modules/selfping/SelfPingerMonitorTest.java
@@ -1,13 +1,10 @@
 /*
- * MultipleRequestsMonitorTest.java
- *
- * Tigase Multi User Chat Component
- * Copyright (C) 2004-2019 "Tigase, Inc." <office@tigase.com>
+ *  Tigase MUC - Multi User Chat component for Tigase
+ * Copyright (C) 2007 Tigase, Inc. (office@tigase.com)
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License,
- * or (at your option) any later version.
+ * the Free Software Foundation, version 3 of the License.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -18,7 +15,6 @@
  * along with this program. Look for COPYING file in the top folder.
  * If not, see http://www.gnu.org/licenses/.
  */
-
 package tigase.muc.modules.selfping;
 
 import org.junit.Assert;
